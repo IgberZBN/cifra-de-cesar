04f0b170355a7ba700a2d969e2ae0d21325bfece
igber <igber_benitez@outlook.com>
2023-01-15 19:22:48 -0300

    Function para cada botao

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,16 @@
 
 const frm = document.querySelector("form");
-const btCrip = document.querySelector("#btCrip");
+const bt1 = document.querySelector("#btCrip"); //referencia ao botao de Criptografar
+const bt2 = document.querySelector("#btDesc"); //referencia ao botao de Descriptografar
 
 //Function para prevenir o submit:
 frm.addEventListener("submit", (e)=>{
     e.preventDefault();
 });
+
+bt1.addEventListener("click", ()=>{
+    console.log("Criptografar");
+})
+bt2.addEventListener("click", ()=>{
+    console.log("Descriptografar");
+})
