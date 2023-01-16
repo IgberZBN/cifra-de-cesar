4189ffc48edc8d9449c203fedf847e88a4c78f83
igber <igber_benitez@outlook.com>
2023-01-15 21:43:24 -0300

    Finalizacao da funcao criptografar

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,7 @@
 const frm = document.querySelector("form");
 const bt1 = document.querySelector("#btCrip"); //referencia ao botao de Criptografar
 const bt2 = document.querySelector("#btDesc"); //referencia ao botao de Descriptografar
+const resp = document.querySelector("#resp");
 
 //Function para prevenir o submit:
 frm.addEventListener("submit", (e)=>{
@@ -18,11 +19,19 @@ bt1.addEventListener("click", ()=>{
         return (n == 32 || n >= 65 && n<=91);
     })
     const textoRotacao = textoFilter.map((n)=>{
+        if(n==32){
+            return n
+        }
         return(n-65+1)%26+65
     })
     texto = textoRotacao.map((n)=>{
         return String.fromCharCode(n);
     })
+    resp.innerHTML = 
+    `
+    <span class="respTitulo">Criptografia do texto:</span><br>
+    ${texto.join('')}
+    `
 })
 bt2.addEventListener("click", ()=>{
     console.log("Descriptografar");
